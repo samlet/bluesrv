bb8a190e35ee9855f695a23e6dd190190a7f0d31
Samlet Wu <xiaofei.wu@gmail.com>
2021-10-24 02:57:35 +0800

    add feeder for rules

--- maintain/feeder.py
+++ maintain/feeder.py
@@ -1,3 +1,5 @@
+import time
+import random
 from typing import Text, Any, Dict, List, Union, Optional
 import json
 
@@ -16,6 +18,10 @@ class Publisher(object):
         self.producer.send(topic, bytes(json_data, "utf-8"))
         self.producer.flush()
 
+    def publish_raw(self, topic, data):
+        self.producer.send(topic, bytes(data, "utf-8"))
+        self.producer.flush()
+
     def publish_multi(self, topic, data_list: List[Any]):
         for data in data_list:
             json_data = json.dumps(data, cls=CustomEncoder, ensure_ascii=False)
@@ -32,7 +38,8 @@ class Publisher(object):
 class Feeder(object):
     def __init__(self):
         # initialize a generator
-        self.fake = Faker()
+        # self.fake = Faker()
+        self.fake = Faker(['it_IT', 'en_US', 'ja_JP', 'zh_CN'])
         self.publisher = Publisher()
 
     def user_interest(self, total=10, publish=False, topic="user_interest"):
@@ -56,6 +63,56 @@ class Feeder(object):
         if publish:
             self.publisher.publish_multi(topic, data_list)
 
+    def proc(self, fn, total:int, publish:bool, topic:str):
+        data_list = []
+        for cur in range(0, total):
+            data = fn()
+            data['id']=cur+1  # add the auto-increment id
+            data_list.append(data)
+            print(cur + 1, data)
+
+        if publish:
+            self.publisher.publish_multi(topic, data_list)
+
+    def user(self, total=10, publish=False, topic="user"):
+        """
+        $ just create user
+        $ just consume user
+        $ python -m feeder user
+        $ python -m feeder user 20 True
+        :return:
+        """
+        self.proc(self.fake.profile, total, publish, topic)
+
+    def send_test_rules(self):
+        """
+        $ python -m feeder send_test_rules
+
+        :return:
+        """
+        self.publisher.publish_raw("rules", """
+[
+  {
+    "name": "adult rule",
+    "description": "when age is greater than 18, then mark as adult",
+    "priority": 1,
+    "condition": "person.age > 18",
+    "actions": [
+      "person.setAdult(true);"
+    ]
+  }
+]
+        """)
+        time.sleep(1)
+        self.publisher.publish("person", {"name": "samlet", "age": 38})
+        self.publisher.publish("person", {"name": "kitty", "age": 8})
+
+        for index in range(0, 100):
+            self.publisher.publish("person", {
+                "name": self.fake.name(),
+                "age": random.randint(1,40),
+            })
+            time.sleep(0.3)
 
 if __name__ == '__main__':
     import fire
